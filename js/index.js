f7285f76029ba70dbe8a75b7a3f76ae71d1e43f3
“Marcela <mpmontiela@gmail.com>
2022-10-12 17:45:39 +0200

    done

--- js/index.js
+++ js/index.js
@@ -13,18 +13,13 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  let total = 0; 
+  const allProducts = document.querySelectorAll(".product");
+  
+  allProducts.forEach(oneProduct => updateSubtotal(oneProduct));
+ 
 }
+
 
 // ITERATION 4
 
